e96e2461f604459d83787a29c70ffd53a4334871
Sayda Yamile Cagua <106682938+SaydaYamileCagua4@users.noreply.github.com>
2023-02-05 12:09:48 -0500

    Update fotoCasual.js

--- HojaVida1/src/components/fotoCasual.js
+++ HojaVida1/src/components/fotoCasual.js
@@ -2,45 +2,36 @@ import React from 'react';
 import Fototogra from './Fototogra';
 import Carousel from 'react-bootstrap/Carousel';
 import '../hoja-estilo/fotogra.css';
+import Fotogra from './Fotogra';
 
 function FotoCasual() {
-    
-    return (
-      <Carousel className= "mt-5">
+  return (
+    <Carousel className= "mt-5">
       <Carousel.Item>
+        <Fototogra 
+        foto='Amor'
+        titulo='Amor'texto='' />
+      </Carousel.Item>
+
+    <Carousel.Item>
       <Fototogra 
-      foto='Amor'
-      titulo='Amor'
-      texto=''
-      />
+      foto='Camilo'
+      titulo='Inocencia'texto='' />
     </Carousel.Item>
 
     <Carousel.Item>
-    <Fototogra 
-    foto='Camilo'
-      titulo='Bebé'
-      texto=''
-      />
+      <Fototogra 
+      foto='enMente'  
+      titulo='Pensamiento'texto='' />
     </Carousel.Item>
 
     <Carousel.Item>
-    <Fototogra 
-    foto='enMente'  
-      titulo='Pensamiento'
-      texto=''
-      />
-    </Carousel.Item>
-    
-    <Carousel.Item>
-    
-    <Fototogra 
+      <Fotogra
       foto='Agua'
-      titulo='Figura'
-      texto=''
-      />
+      titulo='Libre como agua'texto='' />
     </Carousel.Item>
   </Carousel>
-      );
-    }
+  );
+}
 
 export default FotoCasual;
