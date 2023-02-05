5282f70d1ada2bbd00624f51c9850ef8ff5da56f
Sayda Yamile Cagua <106682938+SaydaYamileCagua4@users.noreply.github.com>
2023-02-05 12:15:18 -0500

    Update proyecto.js

--- HojaVida1/src/components/proyecto.js
+++ HojaVida1/src/components/proyecto.js
@@ -7,7 +7,7 @@ function Proyecto (props){
         <p className= 'titulo-proyecto'> <strong>{props.titulo}</strong></p>
         <p className= 'nombre-proyecto'> {props.nombre}</p>
         <p className= 'centro-proyecto'> {props.centro}</p>
-        <p className= 'texto-proyecto'>{props.objetivo}<br></br><br></br>{props.fecha}</p>
+        <p className= 'texto-proyecto'>{props.objetivo}<br /><br />{props.fecha}</p>
         <img className='imagen-proyecto' 
       src={require(`../imagenes/proyectos/proyecto-${props.imagen}.jpg`)}
       alt='Foto proyecto'  />
