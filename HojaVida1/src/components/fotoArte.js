9c67ae2ad0c577e9eff393c1474f00f912f70a34
Sayda Yamile Cagua <106682938+SaydaYamileCagua4@users.noreply.github.com>
2023-02-05 12:09:18 -0500

    Update fotoArte.js

--- HojaVida1/src/components/fotoArte.js
+++ HojaVida1/src/components/fotoArte.js
@@ -2,25 +2,24 @@ import React from 'react';
 import Fototogra from './Fototogra';
 import Carousel from 'react-bootstrap/Carousel';
 import '../hoja-estilo/fotogra.css';
+import Fotogra from './Fotogra';
 
 function FotoArte() {
-    
-    return (
-      <Carousel className= "mt-5">
+  return (
+    <Carousel className= "mt-5">
       <Carousel.Item>
-      <Fototogra 
-      foto='Araña'
-      titulo='Araña'
-      texto='El hombre opta la personificación
-      de dicho animal. Sin mencionar en las civilizaciones egipcias donde la mayor parte
-      de Dioses eran representados hombre-animal..'
-      />
-    
-    </Carousel.Item>
+        <Fototogra 
+        foto='Araña'
+        titulo='Araña'
+        texto='El hombre opta la personificación
+        de dicho animal. Sin mencionar en las civilizaciones egipcias donde la mayor parte
+        de Dioses eran representados hombre-animal..'
+        />
+      </Carousel.Item>
+
     <Carousel.Item>
-   
-    <Fototogra 
-    foto='pezBailarina'
+      <Fototogra 
+      foto='pezBailarina'
       titulo='Pez Bailarina'
       texto='Al desarrollar la técnica de Hombre-animal utilizó una serie de fotografías sobre montadas
       de modelos desnudos, al principio utilice por intervalos un haz de luz con filtros de colores
@@ -30,9 +29,8 @@ function FotoArte() {
     </Carousel.Item>
 
     <Carousel.Item>
-     
-    <Fototogra 
-    foto='pezReina'  
+      <Fototogra 
+      foto='pezReina'  
       titulo='Pez Reina'
       texto='Utilice como base al fotógrafo Brad Wilson por sus bellas imágenes a animales en estudio y
       al ver el resultado tome diferentes imágenes y cree texturas para montarlas sobre ellas, de
@@ -44,19 +42,16 @@ function FotoArte() {
     </Carousel.Item>
     
     <Carousel.Item>
-    
-    <Fototogra 
+      <Fotogra
       foto='Colibri'
-      titulo='Colibri'style={{ height: "30rem", width: "20rem" }}
+      titulo='Colibri'
       texto='El proyecto muestra repetidamente una acción humana convirtiéndola en otro ser vivo
       como parte de la naturaleza.
-        “El humano se identifica con el animal”'
+      “El humano se identifica con el animal”'
       />
     </Carousel.Item>
   </Carousel>
-      
-      );
-    }
+  );
+}
 
-    export default FotoArte;
-
+export default FotoArte;
